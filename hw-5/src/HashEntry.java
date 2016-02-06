b45720738a74bd80eb2cd2a666686a14bfcec299
blackie <xlorkhanx@gmail.com>
2016-02-05 16:51:57 -0800

    adding hw 5

---
+++ hw-5/src/HashEntry.java
@@ -0,0 +1,17 @@
+// The basic entry stored in ProbingHashTable
+
+class HashEntry {
+    Hashable element;   // the element
+    boolean  isActive;  // false is deleted
+    int frequencyCount = 1;  // The number of times this item was inserted
+
+
+    public HashEntry( Hashable e ) {
+        this( e, true );
+    }
+
+    public HashEntry( Hashable e, boolean i ) {
+        element   = e;
+        isActive  = i;
+    }
+}
